5731b297ee52bdd1eb9a54327377f7acb8309a92
Nat <nat@cmmc.io>
2017-08-19 22:01:16 +0700

    publish to the cloud

--- read.py
+++ read.py
@@ -2,18 +2,20 @@ import serial, sys, time, commands, re, os
 import logging
 import paho.mqtt.client as paho
 import time
+import serial.tools.list_ports
+import json
+
 #reload(sys)
 #sys.setdefaultencoding('utf8')
-MQTT = {
-    'default': {
-        'NAME': os.environ.get('MQTT_NAME', ''),
-        'USER': os.environ.get('MQTT_USER', ''),
-        'PASSWORD': os.environ.get('MQTT_PASSWORD', ''),
-        'HOST': os.environ.get('MQTT_HOST', 'mqtt.cmmc.io'),
-        'PORT': os.environ.get('MQTT_PORT', 1883),
-        'TOPIC_1': os.environ.get('MQTT_TOPIC_1', 'CMMC/nat1/espnow'),
-        'TOPIC_2': os.environ.get('MQTT_TOPIC_2', 'CMMC/nat2/espnow'),
-    }
+CONFIG = {
+    'NAME': os.environ.get('MQTT_NAME', ''),
+    'USER': os.environ.get('MQTT_USER', ''),
+    'PASSWORD': os.environ.get('MQTT_PASSWORD', ''),
+    'HOST': os.environ.get('MQTT_HOST', 'beta.cmmc.io'),
+    'PORT': os.environ.get('MQTT_PORT', 51883),
+    'PUB_TOPIC': os.environ.get('MQTT_PUB_TOPIC', 'MARU/data/status'),
+    'SERIAL_PORT_PATTERN': os.environ.get('SERIAL_PORT_PATTERN', 'usbserial*'),
+    'SERIAL_BAUD_RATE': os.environ.get('SERIAL_BAUD_RATE', 57600),
 }
 
 
@@ -24,29 +26,30 @@ def on_publish(client, userdata, mid):
 
 client = paho.Client()
 client.on_publish = on_publish
-client.connect(MQTT['default']['HOST'], MQTT['default']['PORT'])
+client.connect(CONFIG['HOST'], CONFIG['PORT'])
 client.loop_start()
 
 device = None
-baud = 9600
 
 if not device:
-    devicelist = commands.getoutput("ls /dev/ttyAMA*")
-    if devicelist[0] == '/':
-        device = devicelist
-    if not device:
+    matched_ports = serial.tools.list_ports.grep(CONFIG['SERIAL_PORT_PATTERN'])
+    if matched_ports: 
+        device = [x[0] for x in matched_ports][0]
+        print "Success: matched device = %s"% device
+    else:
         print "Fatal: Can't find usb serial device."
         sys.exit(0);
-    else:
-        print "Success: device = %s"% device
 
-ser = serial.Serial(
-    port=device,
-    baudrate=baud,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS
-)
+try: 
+    ser = serial.Serial(
+        port=device,
+        baudrate=CONFIG['SERIAL_BAUD_RATE'],
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS)
+except Exception, e:
+    print str(e)
+    exit()
 
 #https://stackoverflow.com/a/27628622
 def readline(a_serial, eol=b'\r\n'):
@@ -72,22 +75,16 @@ while True:
         line = readline(ser)
         line_str = bytes(line)
         line_hex = str2hexstr(line_str)
-        # print "len = %d" % len(line_str)
-        # b_header = line[0:2]
-        # b_mac1 = line[2:8]
-        # b_mac2 = line[8:14]
-        # msglen = line[14]
-        # msg = line[15:15+msglen]
-        # print str2hexstr(bytes(b_header))
-        # print str2hexstr()
-        # print msglen
-        # print str2hexstr(bytes(msg))
-        # print line_str
-        print str2hexstr(line_str)
-        print 'topic1 = %s' %MQTT['default']['TOPIC_1']
-        print 'topic2 = %s' %MQTT['default']['TOPIC_2']
-        (rc, mid) = client.publish(MQTT['default']['TOPIC_1'], line, qos=0)
-        # (rc, mid) = client.publish(MQTT['default']['TOPIC_2'], line, qos=0)
+        print ">>> " + str2hexstr(line_str)
+        print ">>> type = %d " %line[2]
+        print ">>> len = %d " %line[3]
+
+        if line[3] > 0:
+            json_string = line[4:4+line[3]]
+            print ">>> text = %s " %json_string 
+            if line[2] == 0x03: 
+                print 'pubtopic = %s' %CONFIG['PUB_TOPIC']
+                (rc, mid) = client.publish(CONFIG['PUB_TOPIC'], json_string, qos=0, retain=True)
     except Exception as e:
         print e
     except KeyboardInterrupt:
